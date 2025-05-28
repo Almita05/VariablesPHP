04a12b9f72c567cf179f0f7eee9a40aa2e8dcf09
Alma <martinezlanderoalmalilia@gmail.com>
2025-05-28 12:35:56 -0600

    Nuevo archivo con las operaciones básicas

---
+++ ejercicio05.php
@@ -0,0 +1,10 @@
+<?php
+$a = 8;
+$b = 2;
+
+echo "Suma: " . ($a + $b) . "<br>";
+echo "Resta: " . ($a - $b) . "<br>";
+echo "Multiplicación: " . ($a * $b) . "<br>";
+echo "División: " . ($a / $b) . "<br>";
+
+?>
