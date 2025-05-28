d51684d8fc6a210faa60111811a49f98fee8810c
Alma <martinezlanderoalmalilia@gmail.com>
2025-05-28 12:05:20 -0600

    Ejercicio para conocer el simbolo de ==

---
+++ ejercicio04.php
@@ -0,0 +1,13 @@
+<?php 
+
+$contraseniaGuardada = "admin123";
+$contraseniaIngresada = "123";
+
+if($contraseniaGuardada == $contraseniaIngresada){
+    echo "La contraseña es correcta";
+}
+else {
+    echo "La contraseña es incorrecta";
+}
+
+?>
