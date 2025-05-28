63dfe647169ba642883b35bdbfa4d2017f693aee
Alma <martinezlanderoalmalilia@gmail.com>
2025-05-28 14:02:47 -0600

    Archivo con el operador terniario

---
+++ ejercicio09.php
@@ -0,0 +1,8 @@
+<?php
+//En este programa utiliza la siguiente condicion
+//el operador terniario permite comparar
+//la condicion seguido de ? junto con "condicion si es verdadera" y la condicion si es false
+$logueado = true;
+echo $logueado ? "Bienvenido" : "Iniciar sesión";
+
+?>
