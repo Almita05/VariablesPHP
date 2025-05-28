36c0a33538ae5020b50e0d53c5caa83d2c407ce1
Alma <martinezlanderoalmalilia@gmail.com>
2025-05-28 11:12:25 -0600

    Nuevo archivo con nuevas variables

---
+++ ejercicio01.php
@@ -0,0 +1,11 @@
+<?php
+
+$a = 10;
+$b = 10;
+
+if($a > $b)             {
+echo "$a es mayor que $b";
+} elseif ($a < $b){
+     echo "$a es menor que $b" ; } else { echo "$a es igual a $b" ; }
+
+?>
